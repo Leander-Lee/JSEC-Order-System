d19c5eac59f83e126729dd276c912f9d82c3d324
Leander-Lee <leander.marcus.lee@student.ateneo.edu>
2025-07-09 16:32:12 +0800

    Leander's part 2

--- final_project/delivery/customer/views.py
+++ final_project/delivery/customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, OrderModel
 
 class Index(View):
     def get(self, request, *args, **kwargs):
@@ -7,4 +8,57 @@ class Index(View):
     
 class About(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'customer/about.html')
+        return render(request, 'customer/about.html') 
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        main = MenuItem.objects.filter(category__name__contains='Main')
+        side = MenuItem.objects.filter(category__name__contains='Side')
+        extra = MenuItem.objects.filter(category__name__contains='Extra')
+        #get item from all the categories
+
+        #pass into context
+        context = {
+            "main": main,
+            'side': side,  
+            'extra': extra,
+        } 
+
+        #render the template
+        return render(request, 'customer/order.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        #get total price after selection
+        price = 0
+        item_id=[]
+
+        #we are trying to get all the selected choices for the order
+        #basically this is the list of all ticked boxes
+        for i in items:
+            menu_item = MenuItem.objects.get(pk=int(i))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+            order_items['items'].append(item_data)
+
+            price += menu_item.price
+            item_id.append(menu_item.pk)
+
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_id)
+
+        context = {
+            'items': order_items['items'], 
+            'price': price
+        }
+
+        #reflect everything back
+        return render(request, 'customer/order_confirmation.html', context)
