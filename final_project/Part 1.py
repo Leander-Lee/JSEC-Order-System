2c307b9f71c7b6814835b0cf7b45bf0b72c1f9d0
Leander-Lee <leander.marcus.lee@student.ateneo.edu>
2025-07-18 13:25:49 +0800

    Delete final_project/Part 1.py

--- final_project/Part 1.py
+++
@@ -1,7 +0,0 @@
-#link to final project:
-#https://prod.liveshare.vsengsaas.visualstudio.com/join?0DD9E7BD06ECBB044EA784610D194C0B6EE9
-
-# References:
-# 1. https://www.youtube.com/watch?v=cpPmBlvtdrs
-# 2. https://www.youtube.com/watch?v=msmtduZfAHo 
-
